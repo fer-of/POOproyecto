09daacc519c0826adf3ce311c99adcfb9cca970c
Fer Ordoñez <111704567+fer-of@users.noreply.github.com>
2024-05-27 22:28:34 -0600

    Adicion del CRUD

---
+++ POOproyecto/POOproyect/src/pooproyect/Elemento.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pooproyect;
+
+public class Elemento {
+    int index;
+    double valor;
+
+    public Elemento(int indice, double valor) {
+        this.index = indice;
+        this.valor = valor;
+    }
+
+    public int getIndice() {
+        return index;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
